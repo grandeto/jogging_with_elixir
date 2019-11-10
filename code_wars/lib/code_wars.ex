5c91b94abe26a4d08eb89c6ed91ce4b2ff09d13e
grandeto <grandeto.com@gmail.com>
2019-11-10 04:30:34 +0200

    CodeWars: Some exercises

--- code_wars/lib/code_wars.ex
+++ code_wars/lib/code_wars.ex
@@ -102,3 +102,230 @@ defmodule MappingAndFiltering do
       end)
   end
 end
+
+defmodule Numbers.Operations do
+    import Integer, only: [is_even: 1, is_odd: 1]
+
+    def sumOdd(num_start, num_end),
+        do: sumOdd(num_start, num_end, 0)
+    def sumOdd(num_start, num_end, acc) when num_start == num_end,
+        do: acc
+    def sumOdd(num_start, num_end, acc) when is_even(num_start),
+        do: sumOdd(num_start+1, num_end, acc)
+    def sumOdd(num_start, num_end, acc) when is_odd(num_start),
+        do: sumOdd(num_start+1, num_end, acc+num_start)
+
+    def sumEven(num_start, num_end),
+        do: sumEven(num_start, num_end, 0)
+    def sumEven(num_start, num_end, acc) when num_start == num_end,
+        do: acc
+    def sumEven(num_start, num_end, acc) when is_odd(num_start),
+        do: sumEven(num_start+1, num_end, acc)
+    def sumEven(num_start, num_end, acc) when is_even(num_start),
+        do: sumEven(num_start+1, num_end, acc+num_start)
+end
+
+defmodule ModulePlayground do
+    import IO, only: [puts: 1]
+    import Kernel, except: [inspect: 1]
+
+    alias ModulePlayground.Misc.Util.Math, as: MyMath
+
+    require Integer
+
+    def say_here do
+        inspect "I am Here"
+    end
+
+    def inspect(param1) do
+        puts "Starting output"
+        puts param1
+        puts "Ending output"
+    end
+
+    def print_sum(a,b) do
+        MyMath.add(a,b)
+    end
+
+    def print_is_even(num) do
+        puts "#{num} is even? #{Integer.is_even(num)}"
+    end
+end
+
+defmodule ModulePlayground.Misc.Util.Math do
+    def add(a,b) do
+        a + b
+    end
+end
+
+defmodule Sample.Calendar do
+    import IO
+
+    def is_leap_year(year) when rem(year, 400) == 0, do: true
+    def is_leap_year(year) when rem(year, 100) == 0, do: false
+    def is_leap_year(year) when rem(year, 4) == 0, do: true
+    def is_leap_year(year), do: false
+
+    def day_of_the_week(day) when is_atom(day) do
+        cond do
+            day == :Monday ->
+                "Mon"
+            day == :Tuesday ->
+                "Tu"
+            day == :Wednesday ->
+                "Wed"
+            day == :Thursday ->
+                "Thu"
+            day == :Friday ->
+                "Fri"
+            day == :Saturday ->
+                "Sat"
+            day == :Sunday ->
+                "Sun"
+            true -> "Invalid day"
+        end
+    end
+
+    # Switch(case)
+    def day_of_the_week_case(day) when is_atom(day) do
+        case day do
+            :Monday ->
+                "Mon"
+            :Tuesday ->
+                "Tu"
+            :Wednesday ->
+                "Wed"
+            :Thursday ->
+                "Thu"
+            :Friday ->
+                "Fri"
+            :Saturday ->
+                "Sat"
+            :Sunday ->
+                "Sun"
+            _ -> "Invalid day"
+        end
+    end
+
+    # Functioanal approach
+    def day_of_the_week_func(:Monday), do: "Mon"
+    def day_of_the_week_func(:Tuesday), do: "Tue"
+    def day_of_the_week_func(:Wednesday), do: "Wed"
+    def day_of_the_week_func(:Thursday), do: "Thu"
+    def day_of_the_week_func(:Friday), do: "Fri"
+    def day_of_the_week_func(:Saturday), do: "Sat"
+    def day_of_the_week_func(:Sunday), do: "Sun"
+    def day_of_the_week_func(_), do: "Invalid day"
+
+    # Good pattern matching example
+    def describe_date(date) do
+        case date do
+            {1,_,_} -> "Brand new month"
+            {25,12,_} -> "Merry Christmas"
+            {25,month,_} -> "Just #{12 - month} months till Christmas"
+            {31,10,1517} -> "The Reformation is starting"
+            {31,10,_} -> "Happy Helloween"
+            {_,month,_} when month <= 12 -> "Just an avarage day"
+            {_,_,_} -> "Invalid date"
+        end
+    end
+
+    def send_tweet(path) do
+        case path do
+            {:ok, data} -> puts "Tweet sent: #{data}"
+            {:error, error} -> puts "#{error}"
+        end
+    end
+end
+
+defmodule Sample.Enum do
+    def first(list) when length(list) == 0, do: nil
+    def first(list) do
+        hd(list)
+    end
+
+    def first_new([]) do
+        nil
+    end
+    # alternative syntax
+    # def first_new([]), do: nil
+
+    # Pattern matching
+    def first_new([head | _]) do
+        head
+    end
+    # alternative syntax
+    # def first_new(head | _), do: head
+
+    def add(list, val \\ 0) when is_list(list) do
+        [val | list]
+    end
+
+    def map([], _), do: []
+    def map([hd | tail], f) do
+        [f.(hd) | map(tail, f)]
+    end
+
+    # Body recursion 1+1+1+0 = 3 and it doesn't touch order of the list
+    def length1([]), do: 0
+    def length1([_ | tail]) do
+        1 + length1(tail)
+    end
+
+    # Tail recursion - last operation should be recursion.Avoids overflowing the stack. Donwside is reverse order of the lists
+    def tail_length([_ | tail]) do
+        tail_length(tail, 1)
+    end
+    def tail_length([], len) do
+        len
+    end
+    def tail_length([_ | tail], len) do
+        tail_length(tail, len + 1)
+    end
+
+    #
+    # def other_map([head | tail], f),
+    #     do: other_map(tail, f, [f.(head)])
+    # def other_map([], _, result),
+    #     do: result
+    # def other_map([head | tail], f, result),
+    #     do: other_map(tail, f, [f.(head | result)])
+end
+
+defmodule Sample.Lyrics do
+    defp get_sentence(2) do
+        IO.puts "2 beers"
+    end
+    defp get_sentence(1) do
+        IO.puts "1 beer"
+    end
+    defp get_sentence(number) do
+        IO.puts "#{number} beers"
+    end
+
+    # recursion
+    def lyrics(), do: lyrics(100..1)
+    def lyrics(first..last) when first <= last, do: get_sentence(first)
+    def lyrics(first..last) do
+        get_sentence(first)
+        lyrics(first - 1..last)
+    end
+end
+
+defmodule Sample.Utils do
+    def square(a) do
+        a*a
+    end
+
+    def sum(a,b) do
+        a+b
+    end
+
+    def custom_func(a,f) do
+        f.(a)
+    end
+    # Sample.Utils.custom_func(3, &Sample.Utils.square/1)
+    # Sample.Utils.custom_func(3, fn(x) -> IO.puts(x) end) # anonymous func
+    # Sample.Utils.custom_func( 3, &( IO.puts(&1) ) ) # anonymous func short syntax
+end
+
