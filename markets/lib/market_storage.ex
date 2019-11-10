6dc165957ff983d03b7b4af802a2d49e350900a6
grandeto <grandeto.com@gmail.com>
2019-11-10 23:07:09 +0200

    Markets: add market storage

---
+++ markets/lib/market_storage.ex
@@ -0,0 +1,41 @@
+defmodule CryptoStorage do
+
+    defp storage_path(file) when is_bitstring(file) do
+        "storage/crypto/" <> file
+    end
+
+    defp initial_assets_file do
+        "crypto_assets.json"
+    end
+
+    defp default_assets do
+        %{
+            bitcoin: "bitcoin",
+            ethereum: "ethereum",
+            monero: "monero",
+            litecoin: "litecoin",
+            cardano: "cardano"
+        }
+    end
+
+    defp storage_write(file, content) when is_bitstring(file) and is_map(content) do
+        with :ok <- File.mkdir_p(Path.dirname(storage_path(file))) do
+            File.write(storage_path(file), Jason.encode!(content))
+        end
+    end
+
+    def get_initial_assets_list do
+        {status, assets} = File.read(storage_path(initial_assets_file()))
+        case status == :error do
+            :true -> default_assets()
+            _ -> Jason.decode!(assets)
+        end
+    end
+
+    def set_initial_assets_list(assets) when is_map(assets) do
+        case assets == %{} do
+            :true -> storage_write(initial_assets_file(), default_assets())
+            _ -> storage_write(initial_assets_file(), assets)
+        end
+    end
+end
